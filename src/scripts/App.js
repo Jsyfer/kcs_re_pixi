38a53c73f1e1a7e3f37e3d488d57930400758f5a
Jsyfer <58897323+Jsyfer@users.noreply.github.com>
2023-09-23 22:11:38 +0900

    调整开始前进度条加载动画实现方式

--- src/scripts/App.js
+++ src/scripts/App.js
@@ -12,18 +12,21 @@ class Application {
         document.body.appendChild(this.app.view);
         // 创建化加载界面
         const loading = new Loading();
-        // 创建开始载界面
+        // 创建开始界面
         const start = new Start();
         
         // 添加加载界面至主程序
         this.app.stage.addChild(loading.container);
+        // 创建加载界面
+        loading.create()
         // 运行加载界面
-        loading.create().then(() => {
-            // 加载界面完成运行后载入场景管理器
+        loading.update().then(() => {
+            // 加载界面完成运行后显示开始界面
             this.app.stage.addChild(start.container)
             // 删除加载界面
             this.app.stage.removeChildAt(0);
         });
+
         // TODO 开始按钮按下后切换至母港界面
 
     }
--- src/scripts/scene/Loading.js
+++ src/scripts/scene/Loading.js
@@ -6,26 +6,26 @@ export class Loading extends Scene {
     create() {
         this.container.addChild(PIXI.Sprite.from('assets/kcs2/img/title/04.png'));
 
-        const progressBar = new PIXI.Graphics();
-        progressBar.beginFill(0x22a39f).drawRect(0, 0, 965, 25).endFill();
-        progressBar.position.set(118,665);
-        progressBar.scale.x = 0;
-        this.container.addChild(progressBar);
+        this.progressBar = new PIXI.Graphics();
+        this.progressBar.beginFill(0x22a39f).drawRect(0, 0, 965, 25).endFill();
+        this.progressBar.position.set(118,665);
+        this.progressBar.scale.x = 0;
+        this.container.addChild(this.progressBar);
 
         const progressBarBorder = new PIXI.Graphics();
         progressBarBorder.lineStyle(3, 0xFFFFFF).drawRect(0, 0, 965, 25).endFill();
         progressBarBorder.position.set(118,665);
         this.container.addChild(progressBarBorder);
 
+    }
+
+    async update(){
         // 加载进度条动画
-        return new Promise(async function(resolve) {
-            const timer = ms => new Promise(res => setTimeout(res, ms))
-            while (progressBar.scale.x < 0.99) {
-                progressBar.scale.x += 0.05;
-                await timer(100);
-            }
-            resolve();
-        });
+        const timer = ms => new Promise(res => setTimeout(res, ms))
+        while (this.progressBar.scale.x < 0.99) {
+            this.progressBar.scale.x += 0.02;
+            await timer(500);
+        }
     }
 
 }
