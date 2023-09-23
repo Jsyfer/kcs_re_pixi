b437c34f5df2b7d5f4205793ed3cd01dba37d5c1
Jsyfer <58897323+Jsyfer@users.noreply.github.com>
2023-09-23 18:18:54 +0900

    use assets load json spreadsheet

--- src/scripts/game/Start.js
+++ src/scripts/game/Start.js
@@ -1,7 +1,5 @@
 import * as PIXI from "pixi.js";
 import { Scene } from "../system/Scene";
-import title_main from './../../assets/kcs2/img/title/title_main.json'
-import title_main_img from './../../assets/kcs2/img/title/title_main.png'
 
 // 开始界面
 export class Start extends Scene {
@@ -14,18 +12,15 @@ export class Start extends Scene {
             bg.zIndex = -1;
             this.container.addChild(bg);
         });
-        // Create the SpriteSheet from data and image
-        const spritesheet = new PIXI.Spritesheet(
-            PIXI.BaseTexture.from(title_main_img),
-            title_main
-        );
-        
-        // Generate all the Textures asynchronously
-        spritesheet.parse();
-        const logo = new PIXI.Sprite(spritesheet.textures.title_main_3);
-        logo.position.set(775,110);
-        const btn_default = new PIXI.Sprite(spritesheet.textures.title_main_4);
-        btn_default.position.set(650,550);
-        this.container.addChild(logo,btn_default);
+
+        // 添加按钮
+        PIXI.Assets.load("assets/kcs2/img/title/title_main.json").then((data) => {
+            const logo = new PIXI.Sprite(data.textures.title_main_3);
+            logo.position.set(775,110);
+            const btn_default = new PIXI.Sprite(data.textures.title_main_4);
+            btn_default.position.set(650,550);
+            this.container.addChild(logo,btn_default);
+        })
+
     }
 }
