ec1afa7de657aa48e3d5b35a30715650eb62b0e1
Jsyfer <58897323+Jsyfer@users.noreply.github.com>
2024-08-22 21:13:55 +0900

    config proxy for development and production

--- app.py
+++ app.py
@@ -1,29 +1,16 @@
-import os
 from flask import Flask, render_template, send_from_directory
-from flask_cors import CORS, cross_origin
 
-app = Flask(__name__)
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+app = Flask(__name__, template_folder='dist', static_folder='dist/static')
 
 # 渲染主页
 @app.route('/')
-@cross_origin()
 def index():
     return render_template('index.html')
 
-
 # 提供静态文件
 @app.route('/assets/<path:path>')
-@cross_origin()
 def send_assets(path):
-    return send_from_directory(os.path.join(app.root_path, 'assets'), path)
-
-
-@app.route('/dist/<path:path>')
-def send_dist(path):
-    return send_from_directory(os.path.join(app.root_path, 'dist'), path)
-
+    return send_from_directory('assets', path)
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=8000)
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    assetsDir: 'static',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'static/index.js',
+        assetFileNames: 'static/index.[ext]'
+      }
+    }
+  }
 })
