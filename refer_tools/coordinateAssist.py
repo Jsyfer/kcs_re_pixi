b6a39f6f53ae44d7696991f1c4e04f74532bdae1
Jsyfer <58897323+Jsyfer@users.noreply.github.com>
2024-10-14 21:31:30 +0900

    add coordinate tool

---
+++ refer_tools/coordinateAssist.py
@@ -0,0 +1,78 @@
+import json
+from PIL import Image, ImageDraw
+import cv2
+import numpy as np
+
+base_path = "/Users/jsyfer/GitHub/kcs_re_pixi/"
+spritesheet_image = base_path + "assets/kcs2/img/sally/sally_top.png"
+spritesheet_json = base_path + "assets/kcs2/img/sally/sally_top.json"
+refer_image = '/Volumes/file-server/kcs_re/2024-10-14T19.30.47.png'
+samples = 5
+name_list = [
+    "sally_top_8",
+    "sally_top_1",
+    "sally_top_4",
+    "sally_top_10",
+    "sally_top_3",
+    "sally_top_7",
+]
+
+def load_spritesheet(json_file):
+    with open(json_file, 'r') as f:
+        data = json.load(f)
+    return data
+
+def get_images_in_json(name_list):
+    json_data = load_spritesheet(spritesheet_json)
+    spritesheet = Image.open(spritesheet_image)
+    result_images = []
+    for name, data in json_data['frames'].items():
+        x, y = data['frame']['x'], data['frame']['y']
+        width, height = data['frame']['w'], data['frame']['h']
+        sprite = spritesheet.crop((x, y, x + width, y + height))
+        if name in name_list:
+            result_images.append({"name" : name, "image": sprite})
+    return result_images
+
+
+def find_subimage_position(a_path, b_path):
+    # 读取a.png和b.png，包括透明通道（alpha通道）
+    a_img = cv2.imread(a_path, cv2.IMREAD_UNCHANGED)
+    b_img = cv2.imread(b_path, cv2.IMREAD_UNCHANGED)  # b.png (with alpha)
+
+    # 将透明通道和RGB通道分离开
+    a_alpha = a_img[:, :, 3]  # a.png的透明通道
+    a_rgb = a_img[:, :, :3]   # a.png的RGB通道
+    b_rgb = b_img[:, :, :3]   # b.png的RGB通道
+
+    # 创建掩码，只匹配a.png中不透明的区域
+    mask = a_alpha > 0
+
+    # 用模板匹配来找到a.png在b.png中的位置
+    result = cv2.matchTemplate(b_rgb, a_rgb, cv2.TM_CCORR_NORMED, mask=mask.astype(np.uint8))
+
+    # 找到最大匹配值及其位置
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+
+    # 返回找到的坐标
+    return max_loc
+
+
+refer = Image.open(refer_image)
+
+for item in get_images_in_json(name_list):
+    spriteName = item['name']
+    sprite = item['image']
+    sprite_save_path = f"{spriteName}.png"
+    sprite.save(sprite_save_path)
+    position = find_subimage_position(sprite_save_path, refer_image)
+
+    draw = ImageDraw.Draw(refer)
+
+    x = position[0]
+    y = position[1]
+    coordinate_info = f"{spriteName}: {x},{y}"
+    print(coordinate_info)
+    draw.rectangle((x, y, x + sprite.size [0], y + sprite.size [1] ), outline = 'red')
+    draw.text((x +5 , y + 5) , coordinate_info, fill="yellow", font_size=15, stroke_width=2, stroke_fill="black")
+refer.save('result.png')
--- src/scene/port/ShutsugekiPanel.jsx
+++ src/scene/port/ShutsugekiPanel.jsx
@@ -21,12 +21,12 @@ export const ShutsugekiPanel = () => {
             <Sprite image={'assets/kcs2/img/common/bg/016.png'} x={0} y={0} />
             <Sprite texture={commonSpritesheets[67]} x={0} y={104} />
             <Sprite texture={sallyTop[0]} x={195} y={114} />
-            <PixiButton default={sallyTop[8]} hover={sallyTop[9]} x={180} y={175} />
-            <Sprite texture={sallyTop[10]} x={245} y={575} />
-            <PixiButton default={sallyTop[1]} hover={sallyTop[2]} x={515} y={175} />
-            <Sprite texture={sallyTop[3]} x={570} y={575} />
-            <PixiButton default={sallyTop[4]} hover={sallyTop[6]} x={850} y={175} />
-            <Sprite texture={sallyTop[7]} x={905} y={575} />
+            <PixiButton default={sallyTop[8]} hover={sallyTop[9]} x={179} y={174} />
+            <Sprite texture={sallyTop[10]} x={246} y={575} />
+            <PixiButton default={sallyTop[1]} hover={sallyTop[2]} x={516} y={174} />
+            <Sprite texture={sallyTop[3]} x={572} y={575} />
+            <PixiButton default={sallyTop[4]} hover={sallyTop[6]} x={852} y={174} />
+            <Sprite texture={sallyTop[7]} x={909} y={575} />
         </Container>
     );
 };
