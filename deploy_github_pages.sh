ddf7cefaf070d43fcdffb7fd3fea8579cf24e9cd
Jsyfer <58897323+Jsyfer@users.noreply.github.com>
2024-11-13 22:17:38 +0900

    Create deploy_github_pages.sh

---
+++ deploy_github_pages.sh
@@ -0,0 +1,46 @@
+git add kcs2/resources/furniture/normal/494_1648.png -f
+git add kcs2/resources/furniture/normal/502_8118.png -f
+git add kcs2/resources/furniture/outside/window_bg_4-2.png -f
+git add kcs2/resources/furniture/normal/491_9688.png -f
+git add kcs2/resources/furniture/normal/499_8458.png -f
+git add kcs2/resources/furniture/normal/493_4897.png -f
+git add kcs2/resources/furniture/normal/498_8534.png -f
+git add kcs2/resources/ship/full/0538_2823_sullpopastgr.png -f
+git add kcs2/img/common/bg/011.png -f
+git add kcs2/img/common/bg/012.png -f
+git add kcs2/img/common/bg/013.png -f
+git add kcs2/img/common/bg/014.png -f
+git add kcs2/img/common/bg/015.png -f
+git add kcs2/img/common/bg/016.png -f
+git add kcs2/img/common/bg/031.png -f
+git add kcs2/img/title/title2.png -f
+git add kcs2/img/title/title_main.json -f
+git add kcs2/img/port/port_skin_1.json -f
+git add kcs2/img/port/port_sidemenu.json -f
+git add kcs2/img/port/port_ringmenu.json -f
+git add kcs2/img/common/common_main.json -f
+git add kcs2/img/common/common_misc.json -f
+git add kcs2/img/sally/sally_top.json -f
+git add kcs2/img/organize/organize_main.json -f
+git add kcs2/img/title/title_main.png -f
+git add kcs2/img/port/port_skin_1.png -f
+git add kcs2/img/port/port_sidemenu.png -f
+git add kcs2/img/port/port_ringmenu.png -f
+git add kcs2/img/common/common_main.png -f
+git add kcs2/img/common/common_misc.png -f
+git add kcs2/img/sally/sally_top.png -f
+git add kcs2/img/organize/organize_main.png -f
+git add kcs2/resources/font/A-OTF-UDShinGoPro-Light.woff2 -f
+git add kcs2/img/title/01.png -f
+git add kcs2/img/title/02.png -f
+git add kcs2/img/title/03.png -f
+git add kcs2/img/title/04.png -f
+git add kcs2/img/title/05.png -f
+git add kcs2/img/title/06.png -f
+
+git stash
+
+git checkout gh-pages
+
+git stash pop
+
