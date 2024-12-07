09179128267aa7af6a0c2f26ec4adb5d6ad2e34b
Jsyfer <58897323+Jsyfer@users.noreply.github.com>
2024-12-07 20:45:07 +0900

    update supplyPannel

--- src/scene/port/supply/SupplyPanel.jsx
+++ src/scene/port/supply/SupplyPanel.jsx
@@ -12,7 +12,7 @@ export const SupplyPanel = (props) => {
     const [currentFleet, setCurrentFleet] = useState(0);
     const [shipSelection, setShipSelection] = useState([false, false, false, false, false, false, false]);
 
-    const renderShips = useCallback(() => {
+    const render = useCallback(() => {
         if (currentFleet === -1) {
             // TODO render rest ship list
             return <></>
@@ -20,6 +20,7 @@ export const SupplyPanel = (props) => {
             const api_ship = props.portData.api_data.api_ship;
             const fleet = props.portData.api_data.api_deck_port[currentFleet].api_ship;
             let fleetNeedSupply = false;
+            let flightNeedSupply = false;
             fleet.forEach(ship_no => {
                 if (ship_no === -1) {
                     return
@@ -28,6 +29,20 @@ export const SupplyPanel = (props) => {
                 const target_ship_base_info = props.getData.api_data.api_mst_ship.find(item => item.api_id === target_ship.api_ship_id);
                 if (target_ship.api_fuel !== target_ship_base_info.api_fuel_max || target_ship.api_bull !== target_ship_base_info.api_bull_max) {
                     fleetNeedSupply = true;
+                }
+                if (!(target_ship.api_onslot.length === target_ship_base_info.api_maxeq.length && target_ship.api_onslot.every((value, index) => value === target_ship_base_info.api_maxeq[index]))) {
+                    flightNeedSupply = true;
+                }
+            })
+
+            let fuelToSupply = 0
+            let bullToSupply = 0
+            shipSelection.forEach((isSelected, shipIndex) => {
+                if (isSelected && fleet.length > shipIndex) {
+                    const target_ship = api_ship.find(item => item.api_id === fleet[shipIndex]);
+                    const target_ship_base_info = props.getData.api_data.api_mst_ship.find(item => item.api_id === target_ship.api_ship_id);
+                    fuelToSupply += target_ship_base_info.api_fuel_max - target_ship.api_fuel;
+                    bullToSupply += target_ship_base_info.api_bull_max - target_ship.api_bull;
                 }
             })
 
@@ -51,6 +66,72 @@ export const SupplyPanel = (props) => {
                 <SupplyCard fleet={fleet} shipIndex={3} api_ship={api_ship} getData={props.getData} shipSelection={shipSelection} setShipSelection={setShipSelection} x={203} y={442} />
                 <SupplyCard fleet={fleet} shipIndex={4} api_ship={api_ship} getData={props.getData} shipSelection={shipSelection} setShipSelection={setShipSelection} x={203} y={518} />
                 <SupplyCard fleet={fleet} shipIndex={5} api_ship={api_ship} getData={props.getData} shipSelection={shipSelection} setShipSelection={setShipSelection} x={203} y={594} />
+                {/* 燃料・弾薬の補給 */}
+                <Sprite texture={supplyMain[23]} x={900} y={135} />
+                <Sprite texture={commonMain[67]} x={900} y={104} />
+                <Sprite texture={supplyMain[0]} x={923} y={113} />
+                {/* 補給する資材数 */}
+                <Sprite texture={supplyMain[34]} x={918} y={158} />
+                {/* 補給の燃料 */}
+                <Sprite texture={supplyMain[2]} x={918} y={218} />
+                <Sprite texture={commonMisc[23]} x={986} y={570} angle={180} />
+                <Sprite texture={commonMisc[23]} x={985} y={510} />
+                <Sprite texture={commonMisc[17]} x={918} y={346} />
+                <Sprite texture={commonMisc[16]} x={918} y={513} />
+                <Sprite texture={commonMisc[22]} x={918} y={345} />
+                <Text text={fuelToSupply} x={1045} y={300} anchor={{ x: 1, y: 0 }} style={{ fill: 'black', fontSize: 32 }} />
+                {/* 補給の弾薬 */}
+                <Sprite texture={supplyMain[1]} x={1060} y={218} />
+                <Sprite texture={commonMisc[23]} x={1128} y={570} angle={180} />
+                <Sprite texture={commonMisc[23]} x={1127} y={510} />
+                <Sprite texture={commonMisc[7]} x={1060} y={346} />
+                <Sprite texture={commonMisc[16]} x={1060} y={514} />
+                <Sprite texture={commonMisc[22]} x={1060} y={345} />
+                <Text text={bullToSupply} x={1185} y={300} anchor={{ x: 1, y: 0 }} style={{ fill: 'black', fontSize: 32 }} />
+                {/* 艦載機補充 */}
+                <PixiButton
+                    default={supplyMain[15]}
+                    disabled={supplyMain[16]}
+                    isDisabled={!flightNeedSupply}
+                    hover={supplyMain[17]}
+                    action={resetShipSelection}
+                    x={915} y={597}
+                />
+                {/* 艦隊全補給 */}
+                <PixiButton
+                    default={supplyMain[12]}
+                    disabled={supplyMain[13]}
+                    isDisabled={!fleetNeedSupply}
+                    hover={supplyMain[14]}
+                    actionOver={SelectAllShip}
+                    actionOut={resetShipSelection}
+                    action={resetShipSelection}
+                    x={1060} y={597}
+                />
+                {/* 燃料補給 */}
+                <PixiButton
+                    default={supplyMain[9]}
+                    disabled={supplyMain[10]}
+                    isDisabled={!shipSelection.includes(true)}
+                    hover={supplyMain[11]}
+                    action={resetShipSelection}
+                    x={918} y={641} />
+                {/* まとめて補給 */}
+                <PixiButton
+                    default={supplyMain[3]}
+                    disabled={supplyMain[4]}
+                    isDisabled={!shipSelection.includes(true)}
+                    hover={supplyMain[5]}
+                    action={resetShipSelection}
+                    x={979} y={641} />
+                {/* 弾薬補給 */}
+                <PixiButton
+                    default={supplyMain[6]}
+                    disabled={supplyMain[7]}
+                    isDisabled={!shipSelection.includes(true)}
+                    hover={supplyMain[8]}
+                    action={resetShipSelection}
+                    x={1135} y={641} />
             </>
         }
     }, [currentFleet, shipSelection])
@@ -89,40 +170,8 @@ export const SupplyPanel = (props) => {
             <Sprite texture={supplyMain[29]} x={753} y={158} />
             <Text text={"300000"} x={800} y={164} style={{ fill: 'black', fontSize: 28 }} />
 
-            {renderShips()}
+            {render()}
 
-            {/* 燃料・弾薬の補給 */}
-            <Sprite texture={supplyMain[23]} x={900} y={135} />
-            <Sprite texture={commonMain[67]} x={900} y={104} />
-            <Sprite texture={supplyMain[0]} x={923} y={113} />
-            {/* 補給する資材数 */}
-            <Sprite texture={supplyMain[34]} x={918} y={158} />
-            {/* 補給の燃料 */}
-            <Sprite texture={supplyMain[2]} x={918} y={218} />
-            <Sprite texture={commonMisc[23]} x={986} y={570} angle={180} />
-            <Sprite texture={commonMisc[23]} x={985} y={510} />
-            <Sprite texture={commonMisc[17]} x={918} y={346} />
-            <Sprite texture={commonMisc[16]} x={918} y={513} />
-            <Sprite texture={commonMisc[22]} x={918} y={345} />
-            <Text text={"0"} x={1045} y={300} anchor={{ x: 1, y: 0 }} style={{ fill: 'black', fontSize: 32 }} />
-            {/* 補給の弾薬 */}
-            <Sprite texture={supplyMain[1]} x={1060} y={218} />
-            <Sprite texture={commonMisc[23]} x={1128} y={570} angle={180} />
-            <Sprite texture={commonMisc[23]} x={1127} y={510} />
-            <Sprite texture={commonMisc[7]} x={1060} y={346} />
-            <Sprite texture={commonMisc[16]} x={1060} y={514} />
-            <Sprite texture={commonMisc[22]} x={1060} y={345} />
-            <Text text={"0"} x={1185} y={300} anchor={{ x: 1, y: 0 }} style={{ fill: 'black', fontSize: 32 }} />
-            {/* 艦載機補充 */}
-            <PixiButton default={supplyMain[16]} x={915} y={597} />
-            {/* 艦隊全補給 */}
-            <PixiButton default={supplyMain[13]} x={1060} y={597} />
-            {/* 燃料補給 */}
-            <PixiButton default={supplyMain[10]} x={918} y={641} />
-            {/* まとめて補給 */}
-            <PixiButton default={supplyMain[4]} x={979} y={641} />
-            {/* 弾薬補給 */}
-            <PixiButton default={supplyMain[7]} x={1135} y={641} />
         </Container>
     );
 };
