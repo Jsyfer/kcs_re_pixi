c539f11a8d0bb064ca70ccea8bc0e8b924b2e283
Jsyfer <goldensundd@hotmail.com>
2026-06-13 15:18:41 +0900

    fix ship change API to handle ship not in current fleet

--- backend/views/api_req_hensei.py
+++ backend/views/api_req_hensei.py
@@ -12,13 +12,28 @@ def change(request):
     deck_port = DeckPortService.get_deck_port_by_id(request_data["api_id"])
 
     api_ship = deck_port.api_ship or []
-
-    move_from_index = api_ship.index(int(request_data["api_ship_id"]))
+    api_ship_id = int(request_data["api_ship_id"])
     move_to_index = int(request_data["api_ship_idx"])
-    api_ship[move_from_index], api_ship[move_to_index] = (
-        api_ship[move_to_index],
-        api_ship[move_from_index],
-    )
+    if api_ship_id in api_ship:
+        # If the ship is already in current deck, swap the positions
+        move_from_index = api_ship.index(api_ship_id)
+        api_ship[move_from_index], api_ship[move_to_index] = (
+            api_ship[move_to_index],
+            api_ship[move_from_index],
+        )
+    else:
+        # check if ship in another deck
+        all_decks = DeckPortService.get_deck_port()
+        for deck in all_decks:
+            if api_ship_id in deck.get("api_ship", []):
+                move_from_index = deck["api_ship"].index(api_ship_id)
+                move_to_ship_id = api_ship[move_to_index]
+                deck["api_ship"][move_from_index] = move_to_ship_id
+                DeckPortService.update_deck_port_by_id(
+                    deck["api_id"], "api_ship", deck["api_ship"]
+                )
+                break
+        api_ship[move_to_index] = api_ship_id
 
     DeckPortService.update_deck_port_by_id(request_data["api_id"], "api_ship", api_ship)
 
