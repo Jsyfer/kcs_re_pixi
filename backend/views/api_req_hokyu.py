66c1625a3d28721bb8b44de8e66ccf22dad91ad5
Jsyfer <goldensundd@hotmail.com>
2026-06-14 13:23:28 +0900

    added charge API for fuel and bull

--- backend/views/api_req_hokyu.py
+++ backend/views/api_req_hokyu.py
@@ -41,8 +41,34 @@ def charge(request):
                 }
             )
         elif api_kind == "3":
-            print("todo")
+            # 全補給
+            # 燃料与弹药消耗
+            current_fuel_cons = (mst_ship.api_fuel_max or 0) - (ship.api_fuel or 0)
+            current_bull_cons = (mst_ship.api_bull_max or 0) - (ship.api_bull or 0)
+            # 婚舰燃料与弹药：出击补给时，消耗量减少 15%（计算后向下取整）
+            if (ship.api_lv or 0) > 99:
+                current_fuel_cons = int(current_fuel_cons * 0.85)
+                current_bull_cons = int(current_bull_cons * 0.85)
+            fuel_consumption += current_fuel_cons
+            bull_consumption += current_bull_cons
+            # 铝消耗
+            aluminium_consumption += (
+                sum(mst_ship.api_maxeq or []) - sum(ship.api_onslot or [])
+            ) * 5
+            result_ship_list.append(
+                {
+                    "api_bull": mst_ship.api_bull_max,
+                    "api_fuel": mst_ship.api_fuel_max,
+                    "api_id": ship.api_id,
+                    "api_onslot": mst_ship.api_maxeq,
+                }
+            )
     # 更新各资源消耗
+    if api_kind == "3":
+        fuel.api_value = fuel.api_value - fuel_consumption
+        fuel.save()
+        bull.api_value = bull.api_value - bull_consumption
+        bull.save()
     aluminium.api_value = aluminium.api_value - aluminium_consumption
     aluminium.save()
 
